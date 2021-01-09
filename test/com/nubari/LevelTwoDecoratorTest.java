e908fb58fbf5858be6d5e318d82be4c0a3ec36ef
Eyimofe Ogunbiyi <ogunbiyioladapo33@gmail.com>
2021-01-09 19:05:45 +0100

    tests

---
+++ test/com/nubari/LevelTwoDecoratorTest.java
@@ -0,0 +1,45 @@
+package com.nubari;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LevelTwoDecoratorTest {
+
+    Question question;
+    Question levelTwoQuestion;
+
+    @BeforeEach
+    void setUp() {
+        question = new QuestionImpl();
+        levelTwoQuestion = new LevelTwoDecorator(question);
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+    @Test
+    void testThatLevelNumberShouldBeSetToTwoWhenLevelTwoQuestionInitialized() {
+        assertEquals(2, levelTwoQuestion.getLevelNumber());
+    }
+    @Test
+    void testThatLevelLimitShouldBeSetToSevenWhenLevelTwoQuestionInitialized() {
+        assertEquals(7, levelTwoQuestion.getLevelLimit());
+    }
+
+    @Test
+    void generateQuestion() {
+        levelTwoQuestion.generateQuestion();
+        assertNotNull(levelTwoQuestion.getQuestionString());
+    }
+
+    @Test
+    void determineAnswer() {
+        levelTwoQuestion.generateQuestion();
+        System.out.println(levelTwoQuestion.getAnswer().toString());
+
+        assertNotNull(levelTwoQuestion.getAnswer());
+    }
+}
